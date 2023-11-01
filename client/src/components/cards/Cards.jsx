08b41fc0b5c2ec1319d0895af5d8fe2e62fb8840
Samayoa <fsamayoal@gmail.com>
2023-11-01 12:13:43 -0600

    searchbar

--- client/src/components/cards/Cards.jsx
+++ client/src/components/cards/Cards.jsx
@@ -1,8 +1,67 @@
+// import React, { useState, useEffect } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { fetchCountries } from '../../redux/actions';
+// import Card from '../card/Card';
+// import PageNumbers from '../PageNumbers/PageNumbers';
+
+// function Cards() {
+//   const dispatch = useDispatch();
+//   const countries = useSelector((state) => state.countries);
+
+//   useEffect(() => {
+//     // Realizar la solicitud para obtener la lista de países
+//     dispatch(fetchCountries());
+//   }, [dispatch]);
+
+//   // Calcular el número total de páginas después de obtener los países
+//   const totalPages = Math.ceil(countries.length / 10);
+
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const [paginatedCountries, setPaginatedCountries] = useState([]);
+
+//   useEffect(() => {
+//     // Calcular el índice de inicio y fin de los países para la página actual
+//     const startCountryIndex = (currentPage - 1) * 10;
+//     const endCountryIndex = startCountryIndex + 10;
+
+//     // Obtener los países para la página actual
+//     const currentCountries = countries.slice(startCountryIndex, endCountryIndex);
+
+//     // Actualizar el estado de los países paginados
+//     setPaginatedCountries(currentCountries);
+//   }, [countries, currentPage]);
+
+//   const handlePageChange = (pageNumber) => {
+//     setCurrentPage(pageNumber);
+//   };
+
+//   return (
+//     <div>
+//       {paginatedCountries.map((country) => (
+//         <Card key={country.id} country={country} />
+//       ))}
+
+//       <PageNumbers
+//         totalPages={totalPages}
+//         current={currentPage}
+//         onPageChange={handlePageChange}
+//       />
+//     </div>
+//   );
+// }
+
+// export default Cards;
+
+
+
+
+
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCountries } from '../../redux/actions';
 import Card from '../card/Card';
 import PageNumbers from '../PageNumbers/PageNumbers';
+import Searchbar from '../searchbar/Searchbar'; // Asegúrate de proporcionar la ruta correcta
 
 function Cards() {
   const dispatch = useDispatch();
@@ -18,6 +77,7 @@ function Cards() {
 
   const [currentPage, setCurrentPage] = useState(1);
   const [paginatedCountries, setPaginatedCountries] = useState([]);
+  const [searchedCountries, setSearchedCountries] = useState([]);
 
   useEffect(() => {
     // Calcular el índice de inicio y fin de los países para la página actual
@@ -25,24 +85,39 @@ function Cards() {
     const endCountryIndex = startCountryIndex + 10;
 
     // Obtener los países para la página actual
-    const currentCountries = countries.slice(startCountryIndex, endCountryIndex);
+    const currentCountries = searchedCountries.length > 0
+      ? searchedCountries.slice(startCountryIndex, endCountryIndex)
+      : countries.slice(startCountryIndex, endCountryIndex);
 
     // Actualizar el estado de los países paginados
     setPaginatedCountries(currentCountries);
-  }, [countries, currentPage]);
+  }, [countries, currentPage, searchedCountries]);
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
+  const handleSearch = (searchQuery) => {
+    // Realizar la búsqueda y filtrar los países
+    const filteredCountries = countries.filter((country) =>
+      country.nombre.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+    // Actualizar el estado local con los países filtrados
+    setSearchedCountries(filteredCountries);
+
+    // Reiniciar la paginación a la primera página
+    setCurrentPage(1);
+  };
+
   return (
     <div>
+      <Searchbar onSearch={handleSearch} />
       {paginatedCountries.map((country) => (
         <Card key={country.id} country={country} />
       ))}
-
       <PageNumbers
-        totalPages={totalPages}
+        totalPages={searchedCountries.length > 0 ? Math.ceil(searchedCountries.length / 10) : totalPages}
         current={currentPage}
         onPageChange={handlePageChange}
       />
--- client/src/components/searchbar/Searchbar.jsx
+++ client/src/components/searchbar/Searchbar.jsx
@@ -1,12 +1,103 @@
-import Button from "../Button/Button"
 
-function Searchbar (){
-    return(
-        <div>
-            <input placeholder="Ingresa tu busqueda"></input>
-            <Button text='Buscar'></Button>
-        </div>
-    )
+
+// function Searchbar (){
+//     return(
+//         <div>
+//             <input placeholder="Ingresa tu busqueda"></input>
+//             <Button text='Buscar'></Button>
+//         </div>
+//     )
+// }
+
+// export default Searchbar
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { fetchCountries } from '../../redux/actions';
+
+// function Searchbar() {
+//   const dispatch = useDispatch();
+//   const [query, setQuery] = useState('');
+//   const [filteredCountries, setFilteredCountries] = useState([]); // Nuevo estado para los países filtrados
+//   const countries = useSelector((state) => state.countries);
+
+//   useEffect(() => {
+//     dispatch(fetchCountries());
+//   }, [dispatch]);
+
+//   const handleChange = (event) => {
+//     setQuery(event.target.value);
+//   };
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+
+//     if (countries) {
+//       const filtered = countries.filter((country) => {
+//         return country.nombre.toLowerCase().includes(query.toLowerCase());
+//       });
+
+//       // Actualiza el estado con los países filtrados
+//       setFilteredCountries(filtered);
+//     }
+//   };
+
+//   return (
+//     <div className="search-bar">
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           placeholder="Buscar país"
+//           onChange={handleChange}
+//           value={query}
+//         />
+//         <button type="submit">Buscar</button>
+//       </form>
+//       <div className="filtered-countries">
+//         <ul>
+//           {filteredCountries.map((country) => (
+//             <li key={country.id}>{country.nombre}</li>
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Searchbar;
+
+
+
+import React, { useState } from 'react';
+
+function Searchbar({ onSearch }) {
+  const [query, setQuery] = useState('');
+
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSearch(query);
+  };
+
+  return (
+    <div className="search-bar">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Buscar país"
+          onChange={handleChange}
+          value={query}
+        />
+        <button type="submit">Buscar</button>
+      </form>
+    </div>
+  );
 }
 
-export default Searchbar
+export default Searchbar;
